e6b0b0ff67db6b648342ac58238afee9ca46844d
Yordan SB <yordansaldanabarrera@gmail.com>
2022-02-16 20:23:24 -0300

    implementando la funcion DELETE

--- src/routes/index.routes.js
+++ src/routes/index.routes.js
@@ -5,23 +5,49 @@ const router = Router();
 
 router.get("/", async (req, res) => {
   const tasks = await Task.find().lean();
-  console.log(tasks);
+  // console.log(tasks, "++++INDEX++++");
 
   res.render("index", { tasks: tasks });
   // res.render("index");
 });
 router.post("/tasks/add", async (req, res) => {
-  const task = Task(req.body);
-  await task.save();
+  try {
+    const task = Task(req.body);
+    await task.save();
 
-  res.redirect("/");
+    res.redirect("/");
+  } catch (err) {
+    console.log(err);
+  }
 });
 
 router.get("/about", (req, res) => {
   res.render("about");
 });
 
-router.get("/edit", (req, res) => {
-  res.render("edit");
+router.get("/edit/:id", async (req, res) => {
+  try {
+    const task = await Task.findById(req.params.id).lean();
+    // console.log(task, "++++BUSKEDA+++++");
+    res.render("edit", { task });
+  } catch (err) {
+    console.log(err, "++ERROR++");
+  }
 });
+router.post("/edit/:id", async (req, res) => {
+  const { id } = req.params;
+  console.log(id, "++ID", req.body);
+  try {
+    await Task.findByIdAndUpdate(id, req.body);
+    res.redirect("/");
+  } catch (e) {
+    console.log(e.message, " ->ERROR");
+  }
+});
+router.get("/Delete/:id", async (req, res) => {
+  const { id } = req.params;
+  await Task.findByIdAndDelete(id);
+  res.redirect("/");
+});
+
 export default router;
